64d60fa28e642e4262f051bd059c4df79a57fd32
Harit Patel <haritpatel789@gmail.com>
2026-03-11 11:06:37 +0530

    [Harit Patel].Add UC14 TemperatureMeasurement

--- src/main/java/com/QuantityMeasurementApp/QuantityMeasurementApp.java
+++ src/main/java/com/QuantityMeasurementApp/QuantityMeasurementApp.java
@@ -5,20 +5,35 @@ public class QuantityMeasurementApp {
 	public static void main(String[] args) {
 
 		Quantity<LengthUnit> l1 = new Quantity<>(1, LengthUnit.FEET);
+
 		Quantity<LengthUnit> l2 = new Quantity<>(12, LengthUnit.INCH);
 
-		System.out.println(l1.equals(l2));
+		System.out.println("Length equality: " + l1.equals(l2));
 
 		Quantity<WeightUnit> w1 = new Quantity<>(1, WeightUnit.KILOGRAM);
+
 		Quantity<WeightUnit> w2 = new Quantity<>(1000, WeightUnit.GRAM);
 
-		System.out.println(w1.add(w2));
+		System.out.println("Weight addition: " + w1.add(w2));
 
 		Quantity<VolumeUnit> v1 = new Quantity<>(1, VolumeUnit.LITRE);
+
 		Quantity<VolumeUnit> v2 = new Quantity<>(500, VolumeUnit.MILLILITRE);
 
-		System.out.println(v1.subtract(v2));
+		System.out.println("Volume subtraction: " + v1.subtract(v2));
 
-		System.out.println(l1.divide(new Quantity<>(6, LengthUnit.INCH)));
+		Quantity<TemperatureUnit> t1 = new Quantity<>(0, TemperatureUnit.CELSIUS);
+
+		Quantity<TemperatureUnit> t2 = new Quantity<>(32, TemperatureUnit.FAHRENHEIT);
+
+		System.out.println("Temperature equality: " + t1.equals(t2));
+
+		System.out.println("Temperature conversion: " + t1.convertTo(TemperatureUnit.FAHRENHEIT));
+
+		try {
+			t1.add(t2);
+		} catch (UnsupportedOperationException e) {
+			System.out.println("Temperature arithmetic not supported");
+		}
 	}
 }
